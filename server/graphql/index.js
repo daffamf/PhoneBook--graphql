0ec77483e1a1a445d6b522c480cbe47af267125f
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-08-17 12:00:46 +0700

    modified

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -3,7 +3,7 @@ var queryType = require('./queries/');
 var mutations = require('./mutations/index');
 const firebase = require("firebase");
 
-// console.log(mutations)
+
 
 exports.phonebookSchema = new GraphQLSchema({
     query: queryType,
